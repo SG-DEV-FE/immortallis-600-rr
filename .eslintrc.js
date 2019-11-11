4695348c8fb9ba16d64a5628f72ff8c77ecd2efd
stephen.gault <stephen.gault@trelleborg.com>
2019-11-11 16:08:28 +0000

    career sectoin re done, need to implement scrolling carousel of logos

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,25 +4,25 @@ module.exports = {
     es6: true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:prettier/recommended",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
   ],
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 2018,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["react", "prettier"],
+  plugins: ['react'],
   rules: {
-    "react/jsx-uses-react": "error",
-    "react/jsx-uses-vars": "error",
-    "prettier/prettier": "error",
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+    'prettier/prettier': 'error',
   },
 };
--- src/pages/career/Career.js
+++ src/pages/career/Career.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   MDBContainer,
   MDBCarousel,
@@ -11,76 +11,188 @@ import {
   MDBCardImage,
   MDBCardTitle,
   MDBCol,
+  MDBModal,
+  MDBModalBody,
   MDBRow,
   MDBView,
   MDBMask,
   MDBIcon,
+  MDBCollapse,
 } from 'mdbreact';
+import moment from 'moment';
 import data from '../../../src/data';
 
-const Career = () => {
-  const career = data.Employment;
-  return (
-    <section id="career" className="py-5">
-      <MDBContainer>
-        <MDBRow className="mb-3">
-          <MDBCol sm="12">
-            <h5 className="font-weight-bold my-3 w-responsive">
-              Career history - crazy I know
-            </h5>
-          </MDBCol>
-        </MDBRow>
-      </MDBContainer>
-      <MDBContainer>
-        <MDBCarousel
-          activeItem={1}
-          length={10}
-          showControls={true}
-          showIndicators={true}
-          className="z-depth-1 w-100"
-          interval="5000">
-          <MDBCarouselInner>
+class Career extends Component {
+  state = { collapseID: '' };
+
+  toggleCollapse = collapseID => () => {
+    this.setState(prevState => ({
+      collapseID: prevState.collapseID !== collapseID ? collapseID : '',
+    }));
+  };
+
+  render() {
+    const career = data.Employment;
+    return (
+      <section id="career" className="py-5">
+        <MDBContainer>
+          <MDBRow className="mb-3">
+            <MDBCol sm="12">
+              <h5 className="font-weight-bold my-3 w-responsive">
+                Career history - crazy I know
+              </h5>
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
+
+        <MDBContainer>
+          <MDBRow className="text-center" center>
             {career.map((career, i) => {
               return (
-                <MDBCarouselItem key={i} itemId={career.id}>
-                  <MDBView>
-                    <img
-                      src={career.snapShot}
-                      className="d-block w-100"
-                      alt={career.employerName}
-                    />
-                    <MDBMask overlay="black-light" />
-                  </MDBView>
-                  <MDBCarouselCaption>
-                    <MDBCard
-                      className="mx-auto"
-                      style={{ height: '200px', width: 'auto' }}>
-                      <MDBCardImage
-                        src={career.companyLogo}
-                        style={{ width: 'auto', height: '50px' }}
-                        className="mx-auto my-2"
-                      />
-                      <MDBCardBody>
-                        {career.roles.map(roleDetail => {
-                          return (
-                            <MDBCardTitle>{roleDetail.title}</MDBCardTitle>
-                          );
-                        })}
-
-                        <MDBBtn>
-                          <MDBIcon icon="clone" /> Open Me!
-                        </MDBBtn>
-                      </MDBCardBody>
-                    </MDBCard>
-                  </MDBCarouselCaption>
-                </MDBCarouselItem>
+                <>
+                  {career.id !== 1 ? (
+                    <>
+                      <MDBCol key={i} lg="4" md="12" className="mb-4">
+                        <MDBView className="overlay rounded z-depth-1">
+                          <img
+                            src={career.snapShot}
+                            className="d-block w-100"
+                            alt={career.employerName}
+                          />
+                          <MDBMask overlay="black-light" />
+                        </MDBView>
+                        <MDBCard className="pb-0">
+                          <MDBCardTitle className="mt-3 mb-0">
+                            {career.employerName}
+                          </MDBCardTitle>
+                          <MDBCardBody>
+                            {career.roles.map(roleDetail => {
+                              return (
+                                <>
+                                  <p className="mb-0">{roleDetail.title}</p>
+                                  <MDBBtn
+                                    onClick={this.toggleCollapse(career.id)}>
+                                    Role Information{' '}
+                                    <MDBIcon icon="caret-down" />
+                                  </MDBBtn>
+                                  <MDBCollapse
+                                    className="collapse"
+                                    id={career.id}
+                                    isOpen={this.state.collapseID}>
+                                    <p>{roleDetail.description}</p>
+                                    <p>
+                                      {moment(roleDetail.startDate).format(
+                                        'DD-MM-YYYY'
+                                      )}{' '}
+                                      -{' '}
+                                      {roleDetail.endDate !== null ? (
+                                        <span>
+                                          {moment(roleDetail.endDate).format(
+                                            'DD-MM-YYYY'
+                                          )}
+                                        </span>
+                                      ) : (
+                                        <span>Ongoing</span>
+                                      )}
+                                    </p>
+                                  </MDBCollapse>
+                                  <MDBBtn>
+                                    <MDBIcon icon="clone" /> Open Me!
+                                  </MDBBtn>
+                                </>
+                              );
+                            })}
+                          </MDBCardBody>
+                        </MDBCard>
+                      </MDBCol>
+                    </>
+                  ) : (
+                    <MDBCol key={i} lg="9" md="12" className="mb-4">
+                      <MDBView className="overlay rounded z-depth-1">
+                        <img
+                          src={career.snapShot}
+                          className="d-block w-100"
+                          alt={career.employerName}
+                        />
+                        <MDBMask overlay="black-light" />
+                      </MDBView>
+                      <MDBCard className="pb-0">
+                        <MDBCardTitle className="mt-3 mb-0">
+                          {career.employerName}
+                        </MDBCardTitle>
+                        <MDBCardBody>
+                          {career.roles.map(roleDetail => {
+                            return (
+                              <>
+                                <p className="mb-0">{roleDetail.title}</p>
+                                <MDBBtn
+                                  onClick={this.toggleCollapse(career.id)}>
+                                  Role Information <MDBIcon icon="caret-down" />
+                                </MDBBtn>
+                                <MDBCollapse
+                                  className="collapse"
+                                  id={career.id}
+                                  isOpen={this.state.collapseID}>
+                                  <p>{roleDetail.description}</p>
+                                  <p>
+                                    {moment(roleDetail.startDate).format(
+                                      'DD-MM-YYYY'
+                                    )}{' '}
+                                    -{' '}
+                                    {roleDetail.endDate !== null ? (
+                                      <span>
+                                        {moment(roleDetail.endDate).format(
+                                          'DD-MM-YYYY'
+                                        )}
+                                      </span>
+                                    ) : (
+                                      <span>Ongoing</span>
+                                    )}
+                                  </p>
+                                </MDBCollapse>
+                                <MDBBtn>
+                                  <MDBIcon icon="clone" /> Open Me!
+                                </MDBBtn>
+                              </>
+                            );
+                          })}
+                        </MDBCardBody>
+                      </MDBCard>
+                    </MDBCol>
+                  )}
+                </>
               );
             })}
-          </MDBCarouselInner>
-        </MDBCarousel>
-      </MDBContainer>
-    </section>
-  );
-};
+          </MDBRow>
+        </MDBContainer>
+      </section>
+      /* 
+      <MDBCarousel
+        activeItem={1}
+        length={10}
+        showControls={false}
+        showIndicators={false}
+        className="z-depth-1 w-100"
+        interval="5000">
+        <MDBCarouselInner>
+          {career.map((career, i) => {
+            return (
+              <MDBCarouselItem className="col-md-3" key={i} itemId={career.id}>
+                <MDBView>
+                  <img
+                    src={career.snapShot}
+                    className="d-block w-100"
+                    alt={career.employerName}
+                  />
+                  <MDBMask overlay="black-light" />
+                </MDBView>
+              </MDBCarouselItem>
+            );
+          })}
+        </MDBCarouselInner>
+      </MDBCarousel> */
+    );
+  }
+}
 
 export default Career;
