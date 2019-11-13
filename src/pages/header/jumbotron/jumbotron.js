cfd3975d9a47407e7d4553b1fc3463cba03ff67b
stephen.gault <stephen.gault@trelleborg.com>
2019-11-13 16:03:26 +0000

    attept to fix missing font awesome

--- src/pages/header/jumbotron/jumbotron.js
+++ src/pages/header/jumbotron/jumbotron.js
@@ -12,26 +12,27 @@ import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 const Jumbotron = () => {
   return (
-    <MDBAnimation type="fadeIn" delay=".3s">
-      <MDBView className="view--bg">
-        <MDBMask className="d-flex justify-content-center align-items-center rgba-gradient">
-          <MDBContainer className="px-md-3 px-sm-0">
+    <MDBAnimation type='fadeIn' delay='.3s'>
+      <MDBView className='view--bg'>
+        <MDBMask className='d-flex justify-content-center align-items-center rgba-gradient'>
+          <MDBContainer className='px-md-3 px-sm-0'>
             <MDBRow>
-              <MDBCol md="12" className="mb-4 white-text text-center">
-                <h3 class="display-3 font-weight-bold white-text mb-0 pt-md-5 pt-5">
+              <MDBCol md='12' className='mb-4 white-text text-center'>
+                <h3 class='display-3 font-weight-bold white-text mb-0 pt-md-5 pt-5'>
                   Stephen Gault
                 </h3>
-                <hr class="hr-light my-4 w-75" />
-                <h4 class="subtext-header mt-2 mb-4">
+                <hr class='hr-light my-4 w-75' />
+                <h4 class='subtext-header mt-2 mb-4'>
                   Software Developer / Graphic Design
                 </h4>
 
-                <div className="mt-4 mx-auto text-center">
-                  <AnchorLink href="#personality" offset="40">
+                <div className='mt-4 mx-auto text-center'>
+                  <AnchorLink href='#personality' offset='40'>
                     <MDBIcon
-                      icon="chevron-circle-down"
-                      className="white-text bounce"
-                      size="2x"
+                      fas
+                      icon='chevron-circle-down'
+                      className='white-text bounce'
+                      size='2x'
                     />
                   </AnchorLink>
                 </div>
