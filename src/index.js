717d887f1962f710266341591d14cafa334370be
stephen.gault <stephen.gault@trelleborg.com>
2019-11-13 15:52:38 +0000

    attept to fix missing font awesome

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom';
 import './main/index.css';
 import App from './main/App';
 import * as serviceWorker from './serviceWorker';
-import '@fortawesome/fontawesome-free/css/all.min.css';
+// import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 
-
 ReactDOM.render(<App />, document.getElementById('root'));
-
 
 serviceWorker.unregister();
