9edade181a1afacca5a75676a293f208301d76a8
stephen.gault <stephen.gault@trelleborg.com>
2019-12-03 12:34:35 +0000

    tweaked package.json to add parser

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,12 @@ module.exports = {
     browser: true,
     es6: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
