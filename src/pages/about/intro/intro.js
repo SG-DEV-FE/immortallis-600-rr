73377d391f7745e756f89202c8dc973c9240ab17
stephen65 <stephen@seblgroup.com>
2020-06-01 13:45:17 +0100

    fix for grammar

--- src/pages/about/intro/intro.js
+++ src/pages/about/intro/intro.js
@@ -25,10 +25,9 @@ const Intro = () => {
                 the picture.
               </h3>
               <p className='text-center mx-auto mb-5 w-responsive'>
-                This is me (song&lsquos in your head now). I built this little
-                old site to showcase my skills and generally show off some of my
-                personality, that as you have no doubt already surmised is on
-                the dry wit side...
+                {
+                  "This is me (song's in your head now). I built this little old site to showcase my skills and generally show off some of my personality, that as you have no doubt already surmised is on the dry wit side..."
+                }
                 <br />
                 Also its a work in progress so its rough around the edges, like
                 all good personal projects are...ahem
@@ -72,23 +71,17 @@ const Intro = () => {
           <MDBCol lg={7}>
             <MDBCol size={12} md={12} xl={10} className='px-3'>
               <p>
-                Now those of you who are viewing this on a mobile phone before
-                using a tablet or pc.... nice phone. If your viewing this on a
-                pc / desktop / laptop then you can probably see the mahoosive
-                (its a word) Space Marine. I believe it is what`&lsquo;`s
-                referred to as declaring if you have any addictions... Yes I do.
-                Its called Warhammer 40K.
+                {
+                  "Now those of you who are viewing this on a mobile phone before using a tablet or pc.... nice phone. If your viewing this on a pc / desktop / laptop then you can probably see the mahoosive (its a word) Space Marine. I believe it is what's referred to as declaring if you have any addictions... Yes I do. Its called Warhammer 40K."
+                }
               </p>
               <p>
                 Seriously its my main hobby apart from Gaming{' '}
                 <MDBIcon fab icon='playstation' /> and{' '}
-                <MDBIcon icon='desktop' />, look at them pretty icons. So gaming
-                is a big part of who I am. I have been gaming since the days of
-                the Atari 2600 (centipede was my go to game). Now its mostly
-                PlayStation 4 or my PC. Gamertags can be found further down the
-                page if you`&lsquo;`re interested in checking me out. Awesome
-                gaming fact I have been credited on the BAFTA winning game
-                Tearaway for the PlayStation Vita.
+                <MDBIcon icon='desktop' />,{' '}
+                {
+                  "look at them pretty icons. So gaming is a big part of who I am. I have been gaming since the days of the Atari 2600 (centipede was my go to game). Now its mostly PlayStation 4 or my PC. Gamertags can be found further down the page if you're interested in checking me out. Awesome gaming fact I have been credited on the BAFTA winning game Tearaway for the PlayStation Vita."
+                }
               </p>
               <p>
                 Motorcycling is a big part off my life also, I commute all year
@@ -99,8 +92,9 @@ const Intro = () => {
                 feels very much a home from home.
               </p>
               <p>
-                I was going to mention my family of 3 boys and the wife but not
-                fair to place them on here so we`&lsquo;`ll skip them for now.
+                {
+                  "I was going to mention my family of 3 boys and the wife but not fair to place them on here so we'll skip them for now."
+                }
               </p>
               <p></p>
             </MDBCol>
