809a82d4ed1762c6ea0c7944242de187910d68c7
stephen65 <stephen@seblgroup.com>
2020-06-03 09:29:15 +0100

    google analytics added

--- src/index.js
+++ src/index.js
@@ -6,6 +6,14 @@ import * as serviceWorker from './serviceWorker';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
+import ReactGA from 'react-ga';
+import auth from './auth.ts'; // Sample authentication provider
+
+const trackingId = 'UA-141751502-1'; // Replace with your Google Analytics tracking ID
+ReactGA.initialize(trackingId);
+ReactGA.set({
+  userId: auth.currentUserId(),
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
