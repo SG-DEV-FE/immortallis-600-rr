80152baf9021b03aeb3c2ba08e45432670bf724d
Stephen Gault <stephengault626@googlemail.com>
2020-08-24 17:31:52 +0100

    Update App.js

--- src/main/App.js
+++ src/main/App.js
@@ -23,7 +23,7 @@ class App extends Component {
           <Career />
           <Testimonial />
           <Hobbies />
-          <Gallery />
+          <!--<Gallery />-->
         </main>
         {/* 
     <Insta />*/}
