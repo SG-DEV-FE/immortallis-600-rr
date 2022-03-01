70804cdbf092a6afacf8e821b23e4a2d18f4efdb
Stephen Gault <stephengault626@googlemail.com>
2022-03-01 11:00:08 +0000

    PC copy update

--- src/constants/index.js
+++ src/constants/index.js
@@ -39,6 +39,6 @@ export const copyrightGW = 'Copyright© Games Workshop PLC';
 export const miniaturesData = '/5ddf97ba040d843991fa90b1/2';
 export const gamingData = '/5ddf98c63da40e6f29914c15/4';
 export const educationData = '/5ddfc8d3040d843991faa69d';
-export const pcData = '/5ddfcac9040d843991faa78c';
+export const pcData = '/5ddfcac9040d843991faa78c/1';
 export const testimonialData = '/5ddfceee040d843991faa96e';
 export const employData = '/5ddfd892040d843991faae29';
