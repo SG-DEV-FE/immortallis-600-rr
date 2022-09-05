dd89fda17593a36ee377f50044ab4a1514dd77b8
Stephen Gault <stephengault626@googlemail.com>
2022-09-05 15:01:53 +0100

    Update axiosConfig.js

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
   baseURL: 'https://api.jsonbin.io/v3/b',
   headers: {
-    'secret-key':
+    'X-Master-Key':
       '$2b$10$xQGSxP.Sl/YEC6whT6FeleCnybDSwYXmgIff8k38r2YDXBIaWtVU.',
   },
 });
