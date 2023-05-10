0c087e6300ba9ae748dc5eb9449bc5d8ea76f932
Stephen Gault <stephengault626@googlemail.com>
2023-05-10 16:31:13 +0100

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,5 @@
 // Links
 export const SGLinkedIn = 'https://www.linkedin.com/in/sg-dev';
-export const SGTwitter = 'https://twitter.com/Groghall';
 export const SGInstagram = 'https://www.instagram.com/fallen_hunter_castiel/';
 export const SGPSN = 'https://psnprofiles.com/F-H-Castiel';
 
