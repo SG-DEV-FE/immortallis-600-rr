997e4aa7667c2d668772fe6d0c691eeaa1e049ab
Stephen Gault <stephen.gault@evoketech.com>
2026-03-24 14:59:11 +0000

    updates

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,18 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.jsx?$/,
+    exclude: [],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
   build: {
     outDir: 'build', // Keep 'build' for Netlify compatibility
   },
